775cd898847291119e60ec7b41ca74c53ccda10d
zjt <2223143694@qq.com>
2024-02-28 18:46:13 -0800

    添加vscode测试

---
+++ rosqt/demo01/build/CMakeFiles/ros.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros.dir/main.cpp.o"
+  "ros"
+  "ros.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
